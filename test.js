f486c05672cd85ae6057bb582facea39893a8f49
Philip Loa <boyet007@gmail.com>
2018-12-10 12:20:09 +0700

    #29 - this

--- test.js
+++ test.js
@@ -1,29 +1,13 @@
-var myArray = new Array()
-myArray[0] = 8;
-myArray[1] = 'hello'
-console.log(myArray)
-
-var myCar = new Object()
-myCar.maxSpeed = 50
-myCar.driver = 'Shaun'
-
-myCar.drive = function() {
-    console.log('now driving')
-}
-
-var myVar = 'hey'
-myCar.drive()
-
-
 var myCar2 = {
     maxSpeed: 75, 
     driver: 'Net Ninja', 
-    drive:function(speed, time) {
-        console.log(speed * time)
+    logDrive:function() {
+        //this object information
+        console.log('driver name is ' + this.driver)
     }
 }
 
 
-console.log(myCar2.drive(50, 3))
+myCar2.logDrive()
 
 
