b08ce72a4e0803de53b5ddf96d39ec544e0bccaf
Philip Loa <boyet007@gmail.com>
2018-12-10 11:41:30 +0700

    #25 - splice and split string

---
+++ test.js
@@ -0,0 +1,15 @@
+var str = "hello world"
+
+//ambil posisi dari nomor 3 sebanyak 9 ( yang ke 9 ga di sertakan )
+var str2 = str.slice(2,9)
+
+//     l l o   w o r
+// 0 1 2 3 4 5 6 7 8 9 
+console.log(str2)
+
+
+var tags = 'meat, ham, salami, prok, beef, chicken'
+console.log(tags)
+
+var tagsArray = tags.split(',')
+console.log(tagsArray)
